76f18c9742cb580c8dd4356b1eb065fc5567f920
Ruslan543 <rusel45612@gmail.com>
2022-08-30 23:55:00 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+// https://forkify-api.herokuapp.com/v2
+
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "e2ad52a8-863f-4bc0-bf9c-b121df02e04d";
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,76 @@
+import View from "./View";
+import icons from "url:../../img/icons.svg"; // Parcel 2
+
+class PaginationView extends View {
+  _parentElement = document.querySelector(".pagination");
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener("click", function (e) {
+      const btn = e.target.closest(".btn--inline");
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = this._calcNumPages();
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
+      return this._generateMarkupNext(curPage);
+    }
+
+    // Last page
+    if (curPage === numPages && numPages > 1) {
+      return this._generateMarkupPrev(curPage);
+    }
+
+    // Other page
+    if (curPage < numPages) {
+      return `${this._generateMarkupPrev(curPage)}${this._generateMarkupNext(
+        curPage
+      )}`;
+    }
+
+    // Page 1, and there are NO other pages
+    return "";
+  }
+
+  _calcNumPages() {
+    return Math.ceil(this._data.results.length / this._data.resultsPerPage);
+  }
+
+  _generateMarkupPrev(curPage) {
+    return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+
+          <span>Page ${curPage - 1}</span>
+        </button>
+      `;
+  }
+
+  _generateMarkupNext(curPage) {
+    return `
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+  }
+}
+
+export default new PaginationView();
